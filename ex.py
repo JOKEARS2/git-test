1244688e4f66d513315aef657b96a7264e7073c5
François roche <francoisroche94@gmail.com>
2022-12-05 13:58:27 +0100

    second commit

--- ex.py
+++ ex.py
@@ -22,20 +22,32 @@
 
 #-----------------------------------
 
-import os
+#import os
 
 
-a = os.listdir("C:\\Users")
-b = 1
+#a = os.listdir("C:\\Users")
+#b = 1
 
-for number,i in enumerate(a) :
+#for number,i in enumerate(a) :
 
-    print(f"{number}. Mon dossier : {i} ")
+#    print(f"{number}. Mon dossier : {i} ")
 
 #for i in a :
  #  print(f"{b} Mon dossier : {i} ")
  #  b += 1
 
-print (a)
+#print (a)
+
+#-----------------------------------
+
+import os
+import sys 
+
+print(sys.argv[0])
+liste = os.listdir("c:\\Users")
+
+for nb, i in enumerate(liste):
+    print (f"{nb}.{i}")
+print (liste)
 
 #-----------------------------------

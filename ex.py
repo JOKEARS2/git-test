ca299bcc50f77516c02ce123ebc0d7806ffd355a
François roche <francoisroche94@gmail.com>
2022-12-05 13:52:45 +0100

    first commit

---
+++ ex.py
@@ -0,0 +1,41 @@
+
+#a = ["a","b","c"]
+#b = 1
+
+#for i in a :
+#    print(f"{b} Ma lettre : {i} ")
+#    b += 1
+
+#-----------------------------------
+
+#for number,i in enumerate(a) :
+
+#    print(f"{number}. Ma lettre : {i} ")
+
+#-----------------------------------
+
+#def mafonction(ami):
+ #   print ("salut les amis")
+
+
+#mafonction("ami")
+
+#-----------------------------------
+
+import os
+
+
+a = os.listdir("C:\\Users")
+b = 1
+
+for number,i in enumerate(a) :
+
+    print(f"{number}. Mon dossier : {i} ")
+
+#for i in a :
+ #  print(f"{b} Mon dossier : {i} ")
+ #  b += 1
+
+print (a)
+
+#-----------------------------------
